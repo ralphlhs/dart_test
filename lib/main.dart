5a6032f65913eebc87812778d99b21826e49b1ce
ralphlhs <ralphlhs@empas.com>
2022-05-19 23:52:56 +0900

    둘째커밋

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,6 @@ void main() {
   final hero1 = hero.Hero(70, '영웅');
   final cleric1 = hero.Cleric(100, '이오');
   print(hero1.name+'과 ${hero1.hp}');
-  print(cleric1.nickname +' '+ cleric1.name);
+  print(cleric1.nickname +'와 '+ cleric1.name);
 }
 
